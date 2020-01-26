1503df55ff19418247b0b85ca45e50890c8926ef
carlosguttemberg <guttembergcarlos@gmail.com>
2020-01-26 18:38:35 -0300

    Removendo dependencias do vue, adicionando bootstrap no projeto, criada tela inicial do projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('body')
+    <div class="jumbotron bg-light border border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title"> Cadastro de Produtos</h5>
+                        <p class="card-text"> 
+                            Tela para cadastrar os produtos vendidos...
+                        </p>
+                        <a href="/produtos" class="btn btn-primary"> Novo produto</a>
+                    </div>
+                </div>
+
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title"> Cadastro de Departamentos</h5>
+                        <p class="card-text"> 
+                            Tela para cadastrar os depatamentos dos produtos...
+                        </p>
+                        <a href="/produtos" class="btn btn-primary"> Novo departamento</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Cadastro de Produtos</title>
+    <meta name="csrf_token" content="{{ csrf_token() }}">
+    <style>
+        body{
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <main role="main">
+            @hasSection ('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
